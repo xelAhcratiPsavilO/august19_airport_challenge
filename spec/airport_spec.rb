49ffb8de86407cdbaa2b9deb2d056fb3000ff0bc
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-08-03 12:52:20 +0100

    Add land functionality to Airport class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,10 @@ require 'airport'
 
 describe Airport do
 
+  subject(:airport) { described_class.new }
+
+    it 'lands a plane' do
+      expect(airport).to respond_to(:land).with(1).argument
+    end
+
 end
