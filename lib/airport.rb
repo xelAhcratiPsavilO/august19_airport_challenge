70e9cf9fe18ffd2884d8e7df31a2e44f42db96ea
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-08-03 13:24:49 +0100

    Add capacity functionality to Airport class

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 class Airport
 
+  def initialize(capacity)
+    @capacity = capacity
+    @hangar = []
+  end
+
   def land(plane)
+    raise 'Airport at full capacity' if @hangar.size >= @capacity
+    @hangar << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,22 @@ require 'airport'
 
 describe Airport do
 
-  subject(:airport) { described_class.new }
+  subject(:airport) { described_class.new(20) }
 
-    it 'lands a plane' do
+    it 'allows a plane to land' do
       expect(airport).to respond_to(:land).with(1).argument
     end
 
-    it 'allows a plant to take off' do
+    it 'allows a plane to take off' do
       expect(airport).to respond_to(:take_off).with(1).argument
+    end
+
+    it 'raises an error when hangar is at full capacity' do
+      plane = Plane.new
+      20.times do
+        airport.land(plane)
+      end
+      expect { airport.land(plane) }.to raise_error 'Airport at full capacity'
     end
 
 end
