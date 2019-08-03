80fd956276a433079b7865ad89c0d0cd7f85143b
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-08-03 13:33:38 +0100

    Refactor airport.rb

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,12 +6,18 @@ class Airport
   end
 
   def land(plane)
-    raise 'Airport at full capacity' if @hangar.size >= @capacity
+    raise 'Airport at full capacity' if full?
     @hangar << plane
   end
 
   def take_off(plane)
 
+  end
+
+  private
+
+  def full?
+    @hangar.size >= @capacity
   end
 
 end
